7278e9ab9e432b5d717cac2d3dcfe9fb0294d4bf
Steve Foley <steve.foley@intercom.io>
2024-10-05 18:38:06 +0100

    fix

--- merge-beta-access-pre-3-oct/scripts/z.bash
+++ merge-beta-access-pre-3-oct/scripts/z.bash
@@ -4,10 +4,10 @@ n=50
 
 i=1
 while true; do
-    app_ids=$(cat app-ids | gtail -n +$i | head -n50)
-    if [[ -z $app_ids ]]; then
+    app_ids=($(cat app-ids | gtail -n +$i | head -n50))
+    if (( ${#app_ids[@]} == 0 )); then
         break;
     fi
-    echo "<a href=\"https://app.intercom.com/a/apps/tx2p130c/companies/segments/all:$(printf '%s\n' $app_ids | ./x.bash | ./y.bash)\">$i - $((i + n))</a><br />"
+    echo "<a href=\"https://app.intercom.com/a/apps/tx2p130c/companies/segments/all:$(printf '%s\n' "${app_ids[@]}" | ./x.bash | ./y.bash)\">$i - $((i + ${#app_ids[@]} - 1))</a><br />"
     ((i += n))
 done
